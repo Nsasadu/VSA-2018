f2ea33d547152c9580f3c143c77f50c970122a2a
Nsasadu <41010463+Nsasadu@users.noreply.github.com>
2018-07-11 13:11:03 -0500

    Add files via upload

---
+++ proj05_hangman.py
@@ -0,0 +1,59 @@
+# Name:
+# Date:
+
+
+# proj05: Hangman
+
+# -----------------------------------
+# Helper code
+# (you don't need to understand this helper code)
+import random
+import string
+
+WORDLIST_FILENAME = "words.txt"
+
+def load_words():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print "Loading word list from file..."
+    # inFile: file
+    inFile = open(WORDLIST_FILENAME, 'r', 0)
+    # line: string
+    line = inFile.readline()
+    # wordlist: list of strings
+    wordlist = string.split(line)
+    print "  ", len(wordlist), "words loaded."
+    return wordlist
+
+def choose_word(wordlist):
+    """
+    wordlist (list): list of words (strings)
+
+    Returns a word from wordlist at random
+    """
+    return random.choice(wordlist)
+
+# end of helper code
+# -----------------------------------
+
+# actually load the dictionary of words and point to it with 
+# the wordlist variable so that it can be accessed from anywhere
+# in the program
+wordlist = load_words()
+
+# your code begins here!
+
+word = wordlist[random.randint(1, 55901)]
+
+print word
+
+word_length = word.len
+
+print word_length
+
+print "Welcome to the game, Hangman!"
+#print "I am thinking of a word that is ", , "letters long"
